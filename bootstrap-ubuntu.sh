4dd55c19caa8aa497ff5d40b040e30a7c7aeecf9
Shingo INADA <mecha.is06@gmail.com>
2016-08-22 18:36:47 +0900

    Update bootstrap-ubuntu.sh

--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -180,35 +180,6 @@ au FileType go nmap <leader>d :GoDef<CR>
 au FileType go nmap <leader>e :e #<CR>
 au FileType go nmap <leader>b :GoBuild<CR>
 au FileType go nmap <leader>i :GoImports<CR>
-
-nmap <F8> :TagbarToggle<CR>
-let g:tagbar_type_go = {  
-    \ 'ctagstype' : 'go',
-    \ 'kinds'     : [
-        \ 'p:package',
-        \ 'i:imports:1',
-        \ 'c:constants',
-        \ 'v:variables',
-        \ 't:types',
-        \ 'n:interfaces',
-        \ 'w:fields',
-        \ 'e:embedded',
-        \ 'm:methods',
-        \ 'r:constructor',
-        \ 'f:functions'
-    \ ],
-    \ 'sro' : '.',
-    \ 'kind2scope' : {
-        \ 't' : 'ctype',
-        \ 'n' : 'ntype'
-    \ },
-    \ 'scope2kind' : {
-        \ 'ctype' : 't',
-        \ 'ntype' : 'n'
-    \ },
-    \ 'ctagsbin'  : 'gotags',
-    \ 'ctagsargs' : '-sort -silent'
-\ }
 EOF2
 
 source ~/.profile
