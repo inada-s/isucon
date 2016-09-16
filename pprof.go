2cd5016e035f5b3e0cb4b88ec59281da5698e500
admin <test@example.com>
2016-09-16 20:26:08 +0900

    update pprof

--- pprof.go
+++ pprof.go
@@ -6,47 +6,79 @@ import (
 	"os"
 	"runtime"
 	"runtime/pprof"
+	"time"
 )
 
 var (
+	enableProfile    = true
+	isProfiling      = false
 	cpuProfileFile   = "/tmp/cpu.pprof"
 	memProfileFile   = "/tmp/mem.pprof"
 	blockProfileFile = "/tmp/block.pprof"
 )
 
+func StartProfile(duration time.Duration) error {
+	f, err := os.Create(cpuProfileFile)
+	if err != nil {
+		return err
+	}
+	if err := pprof.StartCPUProfile(f); err != nil {
+		return err
+	}
+	runtime.SetBlockProfileRate(1)
+	isProfiling = true
+	if 0 < duration.Seconds() {
+		go func() {
+			time.Sleep(duration)
+			err := EndProfile()
+			if err != nil {
+				log.Println(err)
+			}
+		}()
+	}
+	log.Println("Profile start")
+	return nil
+}
+
+func EndProfile() error {
+	if !isProfiling {
+		return nil
+	}
+	isProfiling = false
+	pprof.StopCPUProfile()
+	runtime.SetBlockProfileRate(0)
+	log.Println("Profile end")
+
+	mf, err := os.Create(memProfileFile)
+	if err != nil {
+		return err
+	}
+	pprof.WriteHeapProfile(mf)
+
+	bf, err := os.Create(blockProfileFile)
+	if err != nil {
+		return err
+	}
+	pprof.Lookup("block").WriteTo(bf, 0)
+	return nil
+}
+
 func init() {
 	log.Println("add pprof start end func")
 	http.HandleFunc("/startprof", func(w http.ResponseWriter, r *http.Request) {
-		f, err := os.Create(cpuProfileFile)
+		err := StartProfile(time.Minute)
 		if err != nil {
 			w.Write([]byte(err.Error()))
-			return
+		} else {
+			w.Write([]byte("profile started\n"))
 		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		runtime.SetBlockProfileRate(1)
-		w.Write([]byte("profile started\n"))
 	})
 
 	http.HandleFunc("/endprof", func(w http.ResponseWriter, r *http.Request) {
-		pprof.StopCPUProfile()
-		runtime.SetBlockProfileRate(0)
+		err := EndProfile()
 		w.Write([]byte("profile ended\n"))
-
-		mf, err := os.Create(memProfileFile)
 		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
+			w.Write([]byte(err.Error() + "\n"))
 		}
-		pprof.WriteHeapProfile(mf)
-
-		bf, err := os.Create(blockProfileFile)
-		if err != nil {
-			w.Write([]byte(err.Error()))
-			return
-		}
-		pprof.Lookup("block").WriteTo(bf, 0)
 	})
 }
